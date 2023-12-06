d9731ab8a6e85174947d12ba4bd696371f2eaa88
ColZoel <105737771+ColZoel@users.noreply.github.com>
2023-12-05 19:07:06 -0500

    reformat to main

--- zinco.py
+++ zinco.py
@@ -1,29 +1,19 @@
-"""
-Zinco pipeline main file. All the pieces come together here.
-"""
-
-import os
 from __init__ import __version__
-from utils.parse_config import load_config
-
-print(f'\n\n                             Zinco ㎖\n'
-      f'                ––––––––––––––––––––––––––––––––––\n'
-      f'         🚀 AI Boosted Zinco Data Collection Pipeline 🚀\n'
-      f'            📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚\n'
-      f'                         Version {__version__}  \n'
-      f'                ––––––––––––––––––––––––––––––––––\n\n')
+from pipe import *
+from utils.config import *
 
 
-def pipeline(path=None):
-    """
-    Zinco pipeline main function
-    """
-    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yaml')
-    if path is None:
-        path = load_config(config_path)['input_images']
+if __name__ == '__main__':
 
-    # Step 1: Image segmentation
+    print(f'\n\n                             Zinco ㎖\n'
+          f'                ––––––––––––––––––––––––––––––––––\n'
+          f'         🚀 AI Boosted Zinco Data Collection Pipeline 🚀\n'
+          f'            📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚🦑📚\n'
+          f'                         Version {__version__}  \n'
+          f'                ––––––––––––––––––––––––––––––––––\n\n')
 
+    config = load_config('main')
+    if not horizontal():
+        vertical(path=config['input_images'], output=config['output_type'],
+                 debug=config['debug'], cores=config['cores'])
 
-
-    # Step 2: Text extraction
